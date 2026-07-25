1049e422378fdfa9a750296ee60181d63b37b7bf
roger <roger@roger-deb.roger-deb>
2026-07-25 18:24:50 +0200

    Afegits canvis

---
+++ src/assets/images.js
@@ -0,0 +1,22 @@
+const base = import.meta.env.BASE_URL;
+const img = (file) => `${base}img/${encodeURIComponent(file)}`;
+
+export const IMG = {
+  logoHorizontal:  img("LOGO HORIZONTAL.png"),
+  logoVertical:    img("LOGO VERTICAL.png"),
+  cabecera:        img("CABECERA.png"),
+  titulo:          img("TÍTULO.png"),
+  coleccion:       img("COLECCIÓN..png"),
+
+  bookUnicornios:  img("LIBRO UNICORNIOS ADORABLES.png"),
+  bookDinosaurios: img("LIBRO DINOSAURIOS.png"),
+  bookGranja:      img("LIBRO ANIMALES DE GRANJA.png"),
+  bookMonster:     img("LIBRO MONSTER TRUCKS.png"),
+  bookVehiculos:   img("LIBRO DE VEHÍCULOS DE TRABAJO.png"),
+
+  pinterest:       img("LOGO PINTEREST.png"),
+  instagram:       img("LOGO INSTAGRAM.png"),
+  amazon:          img("LOGO AMAZON.png"),
+  pinterestBN:     img("LOGO PINTEREST B_N.png"),
+  amazonBN:        img("LOGO AMAZON B_N.png"),
+};
